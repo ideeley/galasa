aa05ee4e4d88141434c888d2c60f44cdf734677f
Ian Deeley <ian_deeley@uk.ibm.com>
2025-02-04 12:38:22 +0000

    Minor updates/refactoring of IMS TM properties In particular, moving version parsing from DseImsImpl to DseVersion
    Signed-off-by: Ian Deeley <ian_deeley@uk.ibm.com>

--- modules/managers/galasa-managers-parent/galasa-managers-imstm-parent/dev.galasa.imstm.manager/src/main/java/dev/galasa/imstm/internal/properties/DefaultVersion.java
+++ modules/managers/galasa-managers-parent/galasa-managers-imstm-parent/dev.galasa.imstm.manager/src/main/java/dev/galasa/imstm/internal/properties/DefaultVersion.java
@@ -13,6 +13,25 @@ import dev.galasa.ProductVersion;
 import dev.galasa.imstm.ImstmManagerException;
 import dev.galasa.framework.spi.cps.CpsProperties;
 
+/**
+ * IMS TM Systems - Default Version
+ * 
+ * @galasa.cps.property
+ * 
+ * @galasa.name imstm.default.version
+ * 
+ * @galasa.description Provides the default version of IMS TM systems to a custom provisioner.  
+ * 
+ * @galasa.required Not required for DSE, otherwise requirement is defined by custom provisioner
+ * 
+ * @galasa.default 15.5.0
+ * 
+ * @galasa.valid_values A valid V.R.M version format, eg 15.5.0
+ * 
+ * @galasa.examples 
+ * <code>imstm.default.version=15.5.0</code><br>
+ *
+ */
 public class DefaultVersion extends CpsProperties {
 
     private static final Log logger = LogFactory.getLog(DefaultVersion.class);
@@ -20,14 +39,14 @@ public class DefaultVersion extends CpsProperties {
     public static ProductVersion get() {
         String version = "";
         try {
-            version = getStringWithDefault(ImstmPropertiesSingleton.cps(), "15.2.0", "default", "version");
+            version = getStringWithDefault(ImstmPropertiesSingleton.cps(), "15.5.0", "default", "version");
             return ProductVersion.parse(version);
         } catch (ImstmManagerException e) {
-            logger.error("Problem accessing the CPS for the default IMS version, defaulting to 15.2.0");
-            return ProductVersion.v(15).r(2).m(0);
+            logger.error("Problem accessing the CPS for the default IMS version, defaulting to 15.5.0");
+            return ProductVersion.v(15).r(5).m(0);
         } catch (ManagerException e) {
-            logger.error("Failed to parse default IMS version '" + version + "', defaulting to 15.2.0");
-            return ProductVersion.v(15).r(2).m(0);
+            logger.error("Failed to parse default IMS version '" + version + "', defaulting to 15.5.0");
+            return ProductVersion.v(15).r(5).m(0);
         }
     }
 }
